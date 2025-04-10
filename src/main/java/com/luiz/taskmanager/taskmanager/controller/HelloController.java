a77ab764f28219e83ea341ef57cb188698717486
Luiz Fernando de Andrade Gadelha <luizfernandodag@gmail.com>
2025-04-10 14:24:08 -0300

    Initial Spring Boot project setup with /api/hello endpoint

---
+++ src/main/java/com/luiz/taskmanager/taskmanager/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.luiz.taskmanager.taskmanager.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloController {
+
+@GetMapping("/api/hello")
+public String sayHello() {
+    return "Hello From Luiz Fernando ";
+}
+
+
+
+}
